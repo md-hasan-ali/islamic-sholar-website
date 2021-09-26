c0f7caf6ccfaab9ccf3e486df0d63a6369458d04
md-hasan-ali <hasanaliringku@gmail.com>
2021-09-26 09:42:41 +0600

    added header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header Component here</h2>
+        <div className='header'>
+            <div className="container">
+                <h1>Create a Top Islamic Scholars Team</h1>
+                <h2>Total Budget : <span className='text-style'>$1550000</span></h2>
+            </div>
         </div>
     );
 };
